ddb750bbb2949c0ad7465f168d15e83ea7a57429
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-07-19 11:32:45 +0000

    First Commit

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,6 @@
 import { feedback } from "../constants";
 import styles from "../style";
-import FeedbackCard from "./FeedbackCard.jsx";
+import FeedbackCard from "./FeedbackCard";
 
 const Testimonials = () => (
   <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
@@ -19,7 +19,7 @@ const Testimonials = () => (
     </div>
 
     <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
-      {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}
+      {feedback.map((card) => (<FeedbackCard key={card.id} {...card} />))}
     </div>
   </section>
 );
